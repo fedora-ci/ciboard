6089db3cf0199463935e4718c114718265a8d609
Matěj Grabovský <mgrabovs@redhat.com>
2023-01-30 14:45:18 +0100

    Port alerts reducers to Redux Toolkit

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -21,7 +21,7 @@
 
 import { combineReducers } from 'redux';
 
-import { alertsReducer } from './alertsReducer';
+import { alertsReducer } from './alertsSlice';
 import { authReducer } from './authSlice';
 import { filtersReducer } from './filtersSlice';
 import { gateArtifactsReducer } from './gateArtifactsReducer';
