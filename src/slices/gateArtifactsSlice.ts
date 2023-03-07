d392f70174f5a6898b1db6628b47a7ef80be6172
Matěj Grabovský <mgrabovs@redhat.com>
2023-03-07 17:57:51 +0100

    Purify gate artifacts slice
    Make the gate artifacts Redux slice pure by removing references to outer
    state (current URL etc.).

--- src/slices/gateArtifactsSlice.ts
+++ src/slices/gateArtifactsSlice.ts
@@ -25,8 +25,6 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { GASetSearchOptionsPayload } from '../actions/types';
 import { ArtifactType } from '../artifact';
 
-const query = new URLSearchParams(window.location.search);
-
 export const BUILD_TYPE_MENU_ITEMS = {
     modularity: 'redhat-module',
     ordinary: 'brew-build',
@@ -36,7 +34,11 @@ export const getTypeFromSelect = (value: string): ArtifactType =>
     _.get(BUILD_TYPE_MENU_ITEMS, value, 'brew-build');
 
 export const getSelectFromType = (type: string) =>
-    _.get(_.invert(BUILD_TYPE_MENU_ITEMS), type, 'brew-build');
+    _.get(
+        _.invert(BUILD_TYPE_MENU_ITEMS),
+        type,
+        'ordinary',
+    ) as keyof typeof BUILD_TYPE_MENU_ITEMS;
 
 export interface StateGatingTests {
     buildType: ArtifactType;
@@ -49,18 +51,24 @@ export interface StateGatingTests {
     sstTeams: string[];
 }
 
+export type StateGatingTestsSerialized = Partial<
+    Pick<StateGatingTests, 'ciSystem' | 'gateTag' | 'packager'> & {
+        btype: keyof typeof BUILD_TYPE_MENU_ITEMS;
+        ic: string;
+        pid: string;
+        sstTeams: string;
+    }
+>;
+
 const INITIAL_STATE: StateGatingTests = {
-    buildType: getTypeFromSelect(query.get('btype') || 'ordinary'),
-    ciSystem: decodeURIComponent(query.get('cisystem') || ''),
-    gateTag: decodeURIComponent(query.get('gatetag') || ''),
-    ignoreCiSystem: decodeURIComponent(query.get('ic') || 'false') === 'true',
-    packager: decodeURIComponent(query.get('packager') || ''),
-    productId: _.toInteger(query.get('pid') || 604),
+    buildType: 'brew-build',
+    ciSystem: '',
+    gateTag: '',
+    ignoreCiSystem: false,
+    packager: '',
+    productId: 604,
     searchEpoch: 1,
-    sstTeams: _.reject(
-        _.split(decodeURIComponent(query.get('teams') || ''), ','),
-        _.isEmpty,
-    ),
+    sstTeams: [],
     /** buildType === artifact type */
 };
 
@@ -70,56 +78,6 @@ export const gateArtifactsSlice = createSlice({
     reducers: {
         gateArtifactsBumpSearchEpoch: (state) => {
             ++state.searchEpoch;
-
-            const url = new URL(window.location.href);
-            if (!_.isEmpty(state.ciSystem)) {
-                url.searchParams.set(
-                    'cisystem',
-                    encodeURIComponent(state.ciSystem),
-                );
-            } else {
-                url.searchParams.delete('cisystem');
-            }
-            if (!_.isEmpty(state.gateTag)) {
-                url.searchParams.set(
-                    'gatetag',
-                    encodeURIComponent(state.gateTag),
-                );
-            } else {
-                url.searchParams.delete('gatetag');
-            }
-            if (!_.isEmpty(state.packager)) {
-                url.searchParams.set(
-                    'packager',
-                    encodeURIComponent(state.packager),
-                );
-            } else {
-                url.searchParams.delete('packager');
-            }
-            if (!_.isEmpty(state.sstTeams)) {
-                url.searchParams.set(
-                    'teams',
-                    encodeURIComponent(state.sstTeams.toString()),
-                );
-            } else {
-                url.searchParams.delete('teams');
-            }
-
-            url.searchParams.set('pid', encodeURIComponent(state.productId));
-
-            /*
-             * Mapping select entries to actual aritfact type, URL contains value
-             * from select entry
-             */
-            const btype =
-                state.buildType === 'redhat-module' ? 'modularity' : 'ordinary';
-            url.searchParams.set('btype', encodeURIComponent(btype));
-            url.searchParams.set(
-                'ic',
-                encodeURIComponent(state.ignoreCiSystem),
-            );
-
-            window.history.pushState(null, 'update search params', url.href);
         },
         gateArtifactsSetSearchOptions: (
             state,
@@ -140,22 +98,8 @@ export const gateArtifactsSlice = createSlice({
              * resultsdb_testcase: {$regex: 'manual'},
              * $and: [ { gate_tag_name: { $type: "string" } }, { gate_tag_name: { $gt: "" } } ] }
              */
-            let type: ArtifactType | undefined;
-            switch (buildType) {
-                case undefined:
-                    break;
-                case 'ordinary':
-                    type = 'brew-build';
-                    break;
-                case 'modularity':
-                    type = 'redhat-module';
-                    break;
-                default:
-                    console.log('Unknown build type:', buildType);
-                    return state;
-            }
             const updateSet: typeof action.payload = {
-                buildType: type,
+                buildType,
                 ciSystem,
                 gateTag,
                 ignoreCiSystem,
@@ -182,3 +126,27 @@ export const { gateArtifactsBumpSearchEpoch, gateArtifactsSetSearchOptions } =
     gateArtifactsSlice.actions;
 
 export const gateArtifactsReducer = gateArtifactsSlice.reducer;
+
+export const selectSerializedGatingState = <
+    S extends { gateArtifacts: StateGatingTests },
+>(
+    state: S,
+) => {
+    const { gateArtifacts } = state;
+    const serialized: StateGatingTestsSerialized = {};
+
+    if (!_.isEmpty(gateArtifacts.ciSystem))
+        serialized.ciSystem = gateArtifacts.ciSystem;
+    if (!_.isEmpty(gateArtifacts.gateTag))
+        serialized.gateTag = gateArtifacts.gateTag;
+    if (!_.isEmpty(gateArtifacts.packager))
+        serialized.packager = gateArtifacts.packager;
+    if (!_.isEmpty(gateArtifacts.sstTeams))
+        serialized.sstTeams = gateArtifacts.sstTeams.join(',');
+
+    serialized.btype = getSelectFromType(gateArtifacts.buildType);
+    serialized.ic = _.toString(gateArtifacts.ignoreCiSystem);
+    serialized.pid = _.toString(gateArtifacts.productId);
+
+    return serialized;
+};
