16c444a6d43f9aad0feb831295e9b99144f09100
Andrei Stepanov <astepano@redhat.com>
2023-09-14 16:48:17 +0200

    test
    Signed-off-by: Andrei Stepanov <astepano@redhat.com>

--- src/components/ArtifactsList.tsx
+++ src/components/ArtifactsList.tsx
@@ -103,14 +103,11 @@ const ArtifactRPMCard = (props: ArtifactRPMCardProps) => {
         </Card>
     );
 };
+
 /**
-            <CardBody isFilled={false}></CardBody>
-            <CardBody>
                 <FlexItem>{hitSource.component}</FlexItem>
                 <FlexItem>{hitSource.buildId}</FlexItem>
                 <FlexItem>{hitSource.brokerMsgIdGateTag}</FlexItem>
-            </CardBody>
- * 
  */
 
 interface ArtifactCardProps {
