76150a45994cb190aa8f3ed596acc674fefb20bc
Andrei Stepanov <astepano@redhat.com>
2023-11-02 16:03:17 +0100

    xxx
    Signed-off-by: Andrei Stepanov <astepano@redhat.com>

--- src/components/PageDetails/Header.tsx
+++ src/components/PageDetails/Header.tsx
@@ -27,6 +27,8 @@ import {
     FlexItem,
     StackItem,
     TextContent,
+    Toolbar,
+    ToolbarContent,
 } from '@patternfly/react-core';
 import { CodeBranchIcon, CubeIcon, UserIcon } from '@patternfly/react-icons';
 
@@ -54,7 +56,6 @@ function ArtifactTitle(props: ArtifactTitleProps) {
     const hasGatingDecision = !_.isNil(artifact.greenwaveDecision);
     const isScratch = isArtifactScratch(artifact);
     const issuer = getArtifacIssuer(artifact);
-
     return (
         <Flex spaceItems={{ default: 'spaceItemsLg' }}>
             <TextContent className="pf-v5-u-mr-auto">
