f6214110ff5d1cb65ebeb3dc83c9bcf6c25a2f7f
Andrei Stepanov <astepano@redhat.com>
2024-01-04 12:38:37 +0100

    Removed unused code
    Signed-off-by: Andrei Stepanov <astepano@redhat.com>

--- src/components/PageDetails/Header.tsx
+++ src/components/PageDetails/Header.tsx
@@ -20,7 +20,7 @@
  */
 
 import _ from 'lodash';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {
     Flex,
     Stack,
@@ -36,7 +36,7 @@ import {
     CodeBranchIcon,
     AngleDoubleLeftIcon,
 } from '@patternfly/react-icons';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import {
     Artifact,
