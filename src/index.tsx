7244f4cd11ff7123f1896b002c82d756de0f10af
Andrei-Stepanov <astepano@redhat.com>
2024-04-24 14:02:32 +0200

    Revert "Update react to 18"
    This reverts commit 32e633c93aa46450802b2d3ec17c7971f5e46ed0.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import './index.css';
@@ -7,14 +7,13 @@ import { store } from './reduxStore';
 import { App } from './components/App';
 import { reportWebVitals } from './reportWebVitals';
 
-const container = document.getElementById('root');
-const root = createRoot(container!);
-root.render(
-    <Provider store={store}>
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    </Provider>,
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
